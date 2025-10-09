8fa7ef4835f73312825a21cba6832f11e3ebff50
Yuriko Matsuo <N00305488@upn.pe>
2025-10-09 00:30:14 -0500

    resultado

--- app.py
+++ app.py
@@ -482,7 +482,7 @@ def guardar():
             sql = """
                 INSERT INTO cuestionario
                     (id_usuario,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21)
-                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
             """
             valores = [
                 id_usuario, 
